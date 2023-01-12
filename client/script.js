706444f9cce1087f40b0d5b998e80e3c165c745c
axelersson <90241829+axelersson@users.noreply.github.com>
2023-01-12 15:14:19 +0100

    Hejsan

--- client/script.js
+++ client/script.js
@@ -1,3 +1,24 @@
 //Vi behöver börja bygga upp en scriptfil där vi har eventlistnare till form, och sätt att läsa av om fält är ifyllda.
 //Kanske någon flik som gör att man kan välja mellan att se formuläret och varorna som finns.
  
+
+todoForm.title.addEventListener('keyup', (e) => validateField(e.target));
+todoForm.title.addEventListener('blur', (e) => validateField(e.target));
+
+todoForm.description.addEventListener('input', (e) => validateField(e.target));
+todoForm.description.addEventListener('blur', (e) => validateField(e.target));
+
+todoForm.dueDate.addEventListener('input', (e) => validateField(e.target));
+todoForm.dueDate.addEventListener('blur', (e) => validateField(e.target));
+
+todoForm.addEventListener('submit', onSubmit);
+
+
+const todoListElement = document.getElementById('todoList');
+
+let titleValid = true;
+let descriptionValid = true;
+let dueDateValid = true;
+let tasksToChange;
+
+const api = new Api('http://localhost:5100/varor');
